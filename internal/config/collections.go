76797ed51d216bb3adc97e06d5e86dc26ab5e4d4
kume1a <kumela011@gmail.com>
2024-09-10 13:41:01 +0400

    simple fields of scheduled ig reels collection

--- internal/config/collections.go
+++ internal/config/collections.go
@@ -14,7 +14,7 @@ func CreateCollections(app *pocketbase.PocketBase) error {
 		return err
 	}
 
-	collection := &models.Collection{
+	igAccountsCollection := &models.Collection{
 		Name:       "igAccounts",
 		Type:       models.CollectionTypeBase,
 		ListRule:   types.Pointer(""),
@@ -60,7 +60,63 @@ func CreateCollections(app *pocketbase.PocketBase) error {
 		),
 	}
 
-	if err := app.Dao().SaveCollection(collection); err != nil {
+	scheduledIGReelsCollection := &models.Collection{
+		Name:       "scheduledIGReels",
+		Type:       models.CollectionTypeBase,
+		ListRule:   types.Pointer(""),
+		ViewRule:   types.Pointer(""),
+		CreateRule: types.Pointer(""),
+		UpdateRule: types.Pointer(""),
+		DeleteRule: types.Pointer(""),
+		Schema: schema.NewSchema(
+			&schema.SchemaField{
+				Name:     "startAt",
+				Type:     schema.FieldTypeDate,
+				Required: true,
+			},
+			&schema.SchemaField{
+				Name:     "intervalInSeconds",
+				Type:     schema.FieldTypeNumber,
+				Required: true,
+				Options: &schema.NumberOptions{
+					Min:       types.Pointer(1.0),
+					NoDecimal: true,
+				},
+			},
+			&schema.SchemaField{
+				Name:     "title",
+				Type:     schema.FieldTypeText,
+				Required: true,
+				Options: &schema.TextOptions{
+					Max: types.Pointer(255),
+				},
+			},
+			&schema.SchemaField{
+				Name:     "caption",
+				Type:     schema.FieldTypeText,
+				Required: true,
+				Options: &schema.TextOptions{
+					Max: types.Pointer(65535),
+				},
+			},
+			&schema.SchemaField{
+				Name:     "igAccount",
+				Type:     schema.FieldTypeRelation,
+				Required: true,
+				Options: &schema.RelationOptions{
+					MaxSelect:     types.Pointer(1),
+					CollectionId:  igAccountsCollection.Id,
+					CascadeDelete: true,
+				},
+			},
+		),
+	}
+
+	if err := app.Dao().SaveCollection(igAccountsCollection); err != nil {
+		return err
+	}
+
+	if err := app.Dao().SaveCollection(scheduledIGReelsCollection); err != nil {
 		return err
 	}
 
